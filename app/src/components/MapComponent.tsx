aac014e74684aefead49e2b3beb71b5c5fe7cf7f
Alessandra Vertrees <alessandravertrees@gmail.com>
2025-07-08 18:48:48 -0400

    checkpointing this for now

--- app/src/components/MapComponent.tsx
+++ app/src/components/MapComponent.tsx
@@ -4,7 +4,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import { useRef, useEffect, useState } from 'react'
 import mapboxgl from 'mapbox-gl'
 import './map.css'
-
+import countries from "world-countries";
 const INITIAL_CENTER : [number, number]= [
     -74.0242,
     40.6941
@@ -13,73 +13,58 @@ const INITIAL_ZOOM = 10.12
   
 
 const MapComponent = (ingredient) => {
-    const [center, setCenter] = useState(INITIAL_CENTER)
-    const [zoom, setZoom] = useState(INITIAL_ZOOM)
+    // const [center, setCenter] = useState(INITIAL_CENTER)
+    // const [zoom, setZoom] = useState(INITIAL_ZOOM)
+    console.log("countries : ", countries[0])
+    const map = useRef<mapboxgl.Map | null>(null);
+    const mapContainer = useRef<HTMLDivElement | null>(null);
 
-    const mapRef = useRef<mapboxgl.Map | null>(null);
-    const mapContainerRef = useRef<HTMLDivElement | null>(null);
-
-    const handleButtonClick = () => {
-        mapRef.current.flyTo({
-          center: INITIAL_CENTER,
-          zoom: INITIAL_ZOOM
-        })
-      }
-      
     useEffect(() => {
-        mapboxgl.accessToken = 'pk.eyJ1IjoiYXZlcnRyZWVzIiwiYSI6ImNrMGIzNjd0MDBxOGczanI3YXRnMndmb28ifQ.vMs6tdz00Q9KCQ_OSWtn8w'
-        mapRef.current = new mapboxgl.Map({
-          container: mapContainerRef.current,
-          center: center,
-          zoom: zoom
+        mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+          container: mapContainer.current,
+          center: [-77.0323, 38.9131],
+          zoom: INITIAL_ZOOM
         });
+    
+        map.current.on('load', function () {
+  // Add source for admin-0 Boundaries.
+  map.current.addSource('admin-0', {
+    type: 'vector',
+    url: 'mapbox://mapbox.enterprise-boundaries-a0-v1'
+  });
+  // Add a layer with boundary lines.
+  map.current.addLayer({
+    id: 'admin-0-line',
+    type: 'line',
+    source: 'admin-0',
+    'source-layer': 'boundaries_admin_0',
+    paint: {
+      'line-color': 'red',
+      'line-width': ['interpolate', ['linear'], ['zoom'], 0, 2, 20, 10]
+    }
+  }, 'waterway-label');
+  // Add a layer with points.
+  map.current.addLayer({
+    id: 'admin-0-circle',
+    type: 'circle',
+    source: 'admin-0',
+    'source-layer': 'points_admin_0',
+    paint: {
+      'circle-color': 'white',
+      'circle-stroke-color': 'black',
+      'circle-stroke-width': ['interpolate', ['linear'], ['zoom'], 0, 2, 20, 6],
+      'circle-radius': ['interpolate', ['linear'], ['zoom'], 0, 2, 20, 20]
+    }
+  }, 'waterway-label');
 
-        mapRef.current?.on('load', () => {
-            mapRef.current?.addSource('counties', {
-                'type': 'vector',
-                'url': 'mapbox://mapbox.82pkq93d'
-            });
-    
-            mapRef.current?.addLayer({
-                'id': 'counties',
-                'type': 'fill',
-                'source': 'counties',
-                'source-layer': 'original',
-                'paint': {
-                    'fill-outline-color': 'rgba(0,0,0,0.1)',
-                    'fill-color': 'rgba(0,0,0,0.1)'
-                },
-                'slot': 'middle'
-            });
         })
-    
-        mapRef.current.on('move', () => {
-          // get the current center coordinates and zoom level from the map
-          const mapCenter = mapRef.current.getCenter()
-          const mapZoom = mapRef.current.getZoom()
-    
-          // update state
-          setCenter([ mapCenter.lng, mapCenter.lat ])
-          setZoom(mapZoom)
-        })
-
-        // display only features with the 'name' property 'USA'
-        // mapRef.current.setFilter('my-layer', ['==', ['get', 'name'], 'USA']);
-        
-        return () => {
-          mapRef.current.remove()
-        }
-    }, [])
+    })
 
     return (
         <>
-          <div className="sidebar">
-            Longitude: {center[0].toFixed(4)} | Latitude: {center[1].toFixed(4)} | Zoom: {zoom.toFixed(2)}
-          </div>
-          <button className='reset-button' onClick={handleButtonClick}>
-            Reset
-        </button>
-          <div id='map-container'  style={{ width: '500px' }} ref={mapContainerRef} />
+          <div id='map-container'  style={{ width: '500px' }} ref={mapContainer} />
         </>
       )
 }
