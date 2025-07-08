319a693d5051be4f077427bd6fb084df792f6865
Alessandra Vertrees <alessandravertrees@gmail.com>
2025-07-08 18:48:48 -0400

    checkpointing this for now

--- app/src/utils/utils.ts
+++ app/src/utils/utils.ts
@@ -1,5 +1,6 @@
 import ingredients from '@/app/src/data/custom/ingredients.json'
+import { Ingredient } from '../types/ingredient'
 
 export const getIngredient = async (id) => {
-  return ingredients.find((obj)=>obj.id === id)
+  return ingredients.find((obj)=>obj.id === id) as Ingredient
 }
