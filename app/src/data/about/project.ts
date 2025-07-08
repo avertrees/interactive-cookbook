9185ca16449a33ae730ca1561e8277599eeb3cb9
Alessandra Vertrees <alessandravertrees@gmail.com>
2025-07-08 18:58:24 -0400

    cleanup

--- app/src/data/about/project.ts
+++
@@ -1,6 +0,0 @@
-export const project = {
-  title: "",
-  description: "Food is the closest connection we have to nature, science, earth, history, and climate. Food grounds us to the elements. The water, soil, and air. Food connects us. And it shows us how we’re connected We’re probably the most disconnected we’ve ever been to our (global) food system and this is an attempt to reconnect us to that story.",
-  resources: "",
-  contributors: ""
-}
