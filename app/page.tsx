39b7e009e70cdfccd3ecd9d1f024968f1954f855
Alessandra Vertrees <alessandravertrees@nypl.org>
2025-07-10 16:22:12 -0400

    get build to pass

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,9 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-// https://panda-css.com/docs/concepts/responsive-design
-// import Grid from "@/app/src/components/Grid"
-// import SearchForm from "@/app/src/components/SearchForm"
-import data from '@/app/src/data/flavorBible/data-formatted-json.json'
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      {/* <SearchForm />
-      <Grid /> */}
-
       <main className={styles.main}>
         <a href="/ingredients" className="button">Go to Ingredients List</a>
       </main>
