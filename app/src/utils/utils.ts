245f5189e8c15929ce9b532844ddc6b6dc354678
Alessandra Vertrees <alessandravertrees@nypl.org>
2025-07-10 16:59:10 -0400

    hell yeah brother

--- app/src/utils/utils.ts
+++ app/src/utils/utils.ts
@@ -1,6 +1,15 @@
-import ingredients from  '@/data/drafts/custom/ingredients_sample.json'
+// import ingredients from  '@/data/drafts/custom/ingredients_sample.json'
 import { Ingredient } from '../types/ingredient'
+import fs from 'fs';
+import path from 'path'
 
-export const getIngredient = async (id) => {
-  return ingredients.find((obj)=>obj.id === id) as Ingredient
+export const getIngredients = () =>{
+  const filePath = path.join(process.cwd(), 'data', 'ingredients', 'merged', `ingredients_1.json`);
+  const data = fs.readFileSync(filePath, 'utf-8');
+  return JSON.parse(data);
 }
+
+export const getIngredientById = async (id: string | number) => {
+  const ingredients = getIngredients();
+  return ingredients.find((obj) => String(obj.id) === String(id));
+};
