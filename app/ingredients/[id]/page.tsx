e5c444c296d759c35758aa0c3f7f0468933fcfac
Alessandra Vertrees <alessandravertrees@gmail.com>
2025-07-10 21:47:39 -0400

    more data

--- app/ingredients/[id]/page.tsx
+++ app/ingredients/[id]/page.tsx
@@ -3,15 +3,11 @@ import IngredientPage from "@/app/src/components/IngredientPage";
 import { getIngredients, getIngredientById } from "@/app/src/utils/utils";
 import { Ingredient } from "@/app/src/types/ingredient";
 import { Metadata } from 'next';
+// import typeof { PageProps }  from 'next'; // <-- new import
+// import { PageProps } from "@/.next/types/app/page";
+// type IngredientProps = PageProps<{ id: string }>
 
-type IngredientProps = {
-  params: {
-    id: string;
-  };
-};
-
-
-const CustomIngredientPage = async ({ params }: IngredientProps) => {
+const CustomIngredientPage = async ({ params }) => {
   const { id } = await params
   console.log("id is: ", id)
   const ingredient = await getIngredientById(id) 
