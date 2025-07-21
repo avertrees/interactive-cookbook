4911375c0a61a646025f85c70ba8a5656b5cf4b1
Alessandra Vertrees <alessandravertrees@gmail.com>
2025-07-21 08:12:59 -0600

    sort on food groups and food subgroups, add headers

--- app/page.tsx
+++ app/page.tsx
@@ -4,29 +4,39 @@ import { getIngredients, getUniqueFoodGroups, getUniqueFoodSubGroups, getUniqueF
 
 export default async function Home() {
   const ingredients = await getIngredients()
-  const foodGroups = getUniqueFoodGroups(ingredients)
-  const foodSubGroups = getUniqueFoodSubGroups(ingredients)
+
+  const foodGroups = getUniqueFoodGroups(ingredients).sort()
+  const foodSubGroups = getUniqueFoodSubGroups(ingredients).sort()
+  
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        {foodGroups?.map((group, index)=>{
-          const href = `/ingredients?food_group=${group}`
-          return (
-            <li key={index}>
-              <a href={href}>{group as any}</a>
-            </li>
-        )
-        })}
-        {foodSubGroups?.map((group, index)=>{
-          const href = `/ingredients?food_subgroup=${group}`
-          return (
-            <li key={index}>
-              <a href={href}>{group as any}</a>
-            </li>
+        <h2>Food Groups</h2>
+        <ul>
+          {foodGroups?.map((group, index)=>{
+            const href = `/ingredients?food_group=${group}&sort=true`
+            return (
+                <li key={index}>
+                  <a href={href}>{group as any}</a>
+                </li>
           )
-        })}
-        <a href="/ingredients" className="button">Go to Ingredients List</a>
-        <a href="/ingredient-clusters" className="button">View Ingredient Clusters</a>
+          })}
+        </ul>
+
+        <h2>Food SubGroups</h2>
+        <ul>
+          {foodSubGroups?.map((group, index)=>{
+            const href = `/ingredients?food_subgroup=${group}&sort=true`
+
+            return (
+                <li key={index}>
+                  <a href={href}>{group as any}</a>
+                </li>
+            )
+          })}
+        </ul>
+        <a href="/ingredients?sort=true" className="button">Go to Ingredients List</a>
+        <a href="/ingredient-clusters?sort=true" className="button">View Ingredient Clusters</a>
       </main>
     </div>
   );
